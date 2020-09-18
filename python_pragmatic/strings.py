84480e746b0ee22c19c01e0d4ed821fdcb683a93
Erik Telepovský <erik.telepovsky@gmail.com>
2020-09-18 16:25:05 +0200

    bigger barcode font size

--- python_pragmatic/strings.py
+++ python_pragmatic/strings.py
@@ -24,12 +24,15 @@ def barcode(code, args=None):
     CODETYPE = 'code39'
     bc = barcode.get_barcode(CODETYPE)
     bc = bc(code, writer=BarcodeImageWriter(), add_checksum=False)
+    dpi = 300
+    module_height = 7.0
     bc.default_writer_options['quiet_zone'] = 6.4
-    bc.default_writer_options['dpi'] = 300
+    bc.default_writer_options['dpi'] = dpi
     bc.default_writer_options['text_distance'] = 1.0
-    bc.default_writer_options['module_height'] = 10.0
+    bc.default_writer_options['module_height'] = module_height
     # bc.default_writer_options['module_width'] = 0.3
-    bc.default_writer_options['font_size'] = int(bc.default_writer_options['dpi'] / 10)
+    font_size = int(dpi / 5)
+    bc.default_writer_options['font_size'] = font_size
 
     # update by custom arguments
     bc.default_writer_options.update(options)
