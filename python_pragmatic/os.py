8b613859a006e630a86188f1dd34366fe219ff94
eriktelepovsky <erik.telepovsky@gmail.com>
2022-01-20 12:10:44 +0100

    python3 support for base64_to_file()

--- python_pragmatic/os.py
+++ python_pragmatic/os.py
@@ -1,4 +1,12 @@
+from base64 import b64decode
+
+
 def base64_to_file(content, file_path):
     f = open(file_path, "wb")
-    f.write(content.decode('base64'))
+
+    try:
+        f.write(content.decode('base64'))  # python 2
+    except AttributeError:
+        f.write(b64decode(content))  # python 3
+
     f.close()
