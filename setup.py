6ef1bcc119270520b65c5b64a7a03be51988c4df
Erik Telepovský <erik.telepovsky@gmail.com>
2022-02-03 10:41:15 +0100

    v1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='python-pragmatic',
-    version='0.5.1',
+    version='1.0.0',
     description='Pragmatic tools and utilities for Python projects',
     long_description=open('README.rst').read(),
     author='Pragmatic Mates',
